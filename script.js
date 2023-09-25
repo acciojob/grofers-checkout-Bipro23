e06012b2adabc176192aa1898da0ecae09f66987
Bipro23 <88981554+Bipro23@users.noreply.github.com>
2023-09-25 20:43:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
+let table = document.getElementsByTagName("table")[0];
 
 const getSum = () => {
-//Add your code here
-  
+    let sum = 0;
+    let priceArr = document.querySelectorAll(".price");
+
+    priceArr.forEach((singleEle) => {
+        sum += Number(singleEle.innerText);
+    })
+
+    let totalTR = document.createElement("tr");
+    totalTR.setAttribute("id", "ans");
+    totalTR.innerText = sum;
+
+    table.appendChild(totalTR);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
